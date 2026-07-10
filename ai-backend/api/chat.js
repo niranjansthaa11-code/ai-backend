3fb4335d0ec070524ea86eb069dce1b2732bef38
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-07-10 13:22:53 +0545

    Update chat.js

--- ai-backend/api/chat.js
+++ ai-backend/api/chat.js
@@ -110,7 +110,7 @@ Language:
 
         //callign the gemini code to funciton p
         const geminiResponse = await fetch(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,            {
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,            {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ contents: geminiContents })
